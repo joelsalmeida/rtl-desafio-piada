d0208c3f84800eadc6d666dfbaf6ede079b81e94
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-11-26 17:03:39 -0300

    Test: Testa se exibe titulo corretamente

--- rtl-desafio-piada-inicial/src/App.test.js
+++ rtl-desafio-piada-inicial/src/App.test.js
@@ -1,12 +1,15 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { findByText, getByRole, render, screen } from '@testing-library/react';
+import App from './App';
 
-describe("Teste do App Jokes", () => {
-  test('exibe o título "Piadas do Chuck Norris" na tela', () => {
-   
+describe('Teste do App Jokes', () => {
+  test('exibe o título "Piadas do Chuck Norris" na tela', async () => {
+    render(<App />);
+
+    const title = await screen.findByText('Piadas do Chuck Norris');
+    expect(title).toBeInTheDocument();
   });
 
-  test("exibe o corpo da piada na tela", async () => {
-   
+  test('exibe o corpo da piada na tela', async () => {
+
   });
 });
